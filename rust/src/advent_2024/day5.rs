3914a752bc6d45bd209eb58b3a4eb9dd75a42038
sunriseL <540729251@qq.com>
2024-12-05 22:28:52 +0800

    fix map to set

--- rust/src/advent_2024/day5.rs
+++ rust/src/advent_2024/day5.rs
@@ -1,14 +1,14 @@
 use std::error::Error;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::collections::HashMap;
+use std::collections::HashSet;
 
 pub fn part1(input: &str) -> i64 {
     let file = File::open(input).unwrap(); // Open the file
     let reader = BufReader::new(file);
     let mut parse_first = true;
     let mut result: i64 = 0;
-    let mut m = HashMap::<(i32, i32), i32>::new();
+    let mut m = HashSet::<(i32, i32)>::new();
     let mut updates : Vec<Vec<i32>> = Vec::new();
     for line in reader.lines() {
         let line = line.unwrap();
@@ -18,7 +18,7 @@ pub fn part1(input: &str) -> i64 {
         }
         if parse_first {
             let numbers: Vec<i32> = line.split('|').filter_map(|s| s.parse().ok()).collect();
-            m.insert((numbers[0], numbers[1]), 1);
+            m.insert((numbers[0], numbers[1]));
         } else {
             let numbers: Vec<i32> = line.split(',').filter_map(|s| s.parse().ok()).collect();
             updates.push(numbers);
@@ -28,7 +28,7 @@ pub fn part1(input: &str) -> i64 {
         let mut valid = true;
         for i in 0..update.len() {
             for j in i..update.len() {
-                if m.get(&(update[j], update[i])).is_some() {
+                if m.contains(&(update[j], update[i])) {
                     valid = false;
                     break;
                 }
@@ -45,7 +45,7 @@ pub fn part2(input: &str) -> i64 {
     let file = File::open(input).unwrap(); // Open the file
     let reader = BufReader::new(file);
     let mut result: i64 = 0;
-    let mut m = HashMap::<(i32, i32), i32>::new();
+    let mut m = HashSet::<(i32, i32)>::new();
     let mut updates : Vec<Vec<i32>> = Vec::new();
     let mut parse_first = true;
     for line in reader.lines() {
@@ -56,7 +56,7 @@ pub fn part2(input: &str) -> i64 {
         }
         if parse_first {
             let numbers: Vec<i32> = line.split('|').filter_map(|s| s.parse().ok()).collect();
-            m.insert((numbers[0], numbers[1]), 1);
+            m.insert((numbers[0], numbers[1]));
         } else {
             let numbers: Vec<i32> = line.split(',').filter_map(|s| s.parse().ok()).collect();
             updates.push(numbers);
@@ -79,7 +79,7 @@ pub fn part2(input: &str) -> i64 {
             let mut count: i32 = 0;
             for j in 0..update.len() {
                 if i == j { continue; }
-                if m.get(&(update[i], update[j])).is_some() {
+                if m.contains(&(update[i], update[j])) {
                     count += 1;
                 } else {
                     count -= 1;
